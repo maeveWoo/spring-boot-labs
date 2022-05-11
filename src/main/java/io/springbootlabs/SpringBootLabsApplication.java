11e5bd131d8492723bd44afb54120bf3a4b3270a
maeve <maeve.woo@kakao.com>
2022-05-11 14:45:01 +0900

    project init
    -SpringBoot2.6.7
    -Java11
    -SpringDataJPA
    -Lombok
    -MariaDB
    -Thymeleaf

---
+++ src/main/java/io/springbootlabs/SpringBootLabsApplication.java
@@ -0,0 +1,13 @@
+package io.springbootlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/springbootlabs/SpringBootLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbootlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
