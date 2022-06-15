7b0cbbb061e799f0d3b48abe1b273a0e631461ca
maeve <maeve.woo@kakao.com>
2022-06-15 10:17:07 +0900

    Spring Security 5.7 WebSecurityConfigurerAdapter deprecated

---
+++ src/main/java/io/springbootlabs/app/web/config/security/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package io.springbootlabs.app.web.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(auth ->
+                        {
+                            try {
+                                auth
+                                        .antMatchers("/login", "/actuator/health", "/test/**")
+                                        .permitAll()
+                                        .antMatchers("/favicon.ico", "/js/**", "/css/**", "/font-awesome/**", "/img/**", "/fonts/**", "/vendor/**", "/error")
+                                        .permitAll()
+                                        .anyRequest().authenticated()
+                                        .and()
+                                        .formLogin()
+                                        .loginPage("/login")
+                                        .defaultSuccessUrl("/")
+                                        .and()
+                                        .exceptionHandling().accessDeniedPage("/error-forbidden");
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
