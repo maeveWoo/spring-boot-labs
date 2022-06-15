958551cb702a9a83d6c2501a2fc0616b5d586987
maeve <maeve.woo@kakao.com>
2022-06-15 23:13:48 +0900

    로그인 여부에 따른 header 메세지 세팅, nav 기능 핸들링

--- src/main/java/io/springbootlabs/app/web/config/security/User.java
+++ src/main/java/io/springbootlabs/app/web/config/security/User.java
@@ -1,10 +1,12 @@
 package io.springbootlabs.app.web.config.security;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.io.Serializable;
 import java.util.Collection;
 
+@Getter
 public class User extends org.springframework.security.core.userdetails.User implements Serializable {
     private String nickName;
     public User(Long id, String password, Collection<? extends GrantedAuthority> authorities, String nickName) {
