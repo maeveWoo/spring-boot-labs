dc1fa438e3bbcc651ea82455a527ca5bb3d694c5
maeve <maeve.woo@kakao.com>
2022-06-16 11:09:32 +0900

    화면 라우팅 세팅.
    - 인사페이지 제거.
    - 불필요한 화면제거 및 content 이동

--- src/main/java/io/springbootlabs/app/web/in/HelloController.java
+++ src/main/java/io/springbootlabs/app/web/in/HelloController.java
@@ -1,12 +1,7 @@
 package io.springbootlabs.app.web.in;
 
-import io.springbootlabs.app.web.dto.Greeting;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @Controller
 public class HelloController {
@@ -17,22 +12,12 @@ public class HelloController {
 
     @GetMapping("/login")
     public String login(){
-        return "login";
+        return "content/login";
     }
 
-    @GetMapping("/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("greeting", new Greeting());
-        return "greeting";
-    }
-
-    @PostMapping("/greeting")
-    public String greetingSubmit(@Valid @ModelAttribute Greeting greeting, BindingResult bindingResult, Model model) {
-        model.addAttribute("greeting", greeting);
-        if (bindingResult.hasErrors()) {
-            return "greeting";
-        }
-        return "result";
+    @GetMapping("/logout")
+    public String logout(){
+        return "index";
     }
 
     @GetMapping("/test")
