89a2ca6782b889eaa6a8b73a1983ad35f59a97ac
maeve <maeve.woo@kakao.com>
2022-06-17 16:56:27 +0900

    User 생성자 nullable false 파라미터 추가.

--- src/main/java/io/springbootlabs/domain/user/User.java
+++ src/main/java/io/springbootlabs/domain/user/User.java
@@ -10,17 +10,22 @@ import java.util.List;
 @Getter
 @Entity
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 200, nullable = false)
     private String password;
 
-    /** 변경가능한 닉네임 **/
+    /**
+     * 변경가능한 닉네임
+     **/
     @Column(length = 20, nullable = false)
     private String nickName;
 
-    /** 최근 업데이트 **/
+    /**
+     * 최근 업데이트
+     **/
     @Column(nullable = false)
     private LocalDateTime lastUpdated;
 
@@ -31,20 +36,23 @@ public class User {
     @Enumerated(EnumType.STRING)
     private List<Role> roles = new ArrayList<>();
 
-    protected User() {}
+    protected User() {
+    }
 
-    private User(List<Role> roles) {
+    private User(List<Role> roles, String password, String nickName) {
         this.lastUpdated = LocalDateTime.now();
         this.createdAt = LocalDateTime.now();
         this.roles = roles;
+        this.password = password;
+        this.nickName = nickName;
     }
 
-    static User createUser() {
-        return new User(List.of(Role.USER));
+    public static User createUser(String password, String nickName) {
+        return new User(List.of(Role.USER), password, nickName);
     }
 
-    static User createAdmin() {
-        return new User(List.of(Role.ADMIN));
+    public static User createAdmin(String password, String nickName) {
+        return new User(List.of(Role.ADMIN), password, nickName);
     }
 
     private void addRoles(Role role) {
--- src/test/java/io/springbootlabs/domain/user/UserTest.java
+++ src/test/java/io/springbootlabs/domain/user/UserTest.java
@@ -9,14 +9,20 @@ class UserTest {
     @Test
     @DisplayName("User 생성시 User 권한만 세팅")
     public void userCreate() {
-        User user = User.createUser();
+        User user = User.createUser("test", "test");
+
         assertThat(user.getRoles()).containsOnly(Role.USER);
+        assertThat(user.getNickName()).isNotNull();
+        assertThat(user.getPassword()).isNotNull();
     }
 
     @Test
     @DisplayName("Admin 생성시 User 권한만 세팅")
     public void userAdmin() {
-        User admin = User.createAdmin();
+        User admin = User.createAdmin("test", "test");
+
         assertThat(admin.getRoles()).containsOnly(Role.ADMIN);
+        assertThat(admin.getNickName()).isNotNull();
+        assertThat(admin.getPassword()).isNotNull();
     }
 }
